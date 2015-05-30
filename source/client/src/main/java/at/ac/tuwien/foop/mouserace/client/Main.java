80c5fe3cc61482fee3e64431c4e669c8c41b163f
Klaus Krapfenbauer <klaus.krapfenbauer@gmail.com>
2015-05-30 11:24:41 +0200

    Initial project setup

---
+++ source/client/src/main/java/at/ac/tuwien/foop/mouserace/client/Main.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.foop.mouserace.client;
+
+/**
+ * Created by klaus on 5/30/15.
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
---
+++ source/server/src/main/java/at/ac/tuwien/foop/mouserace/server/Main.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.foop.mouserace.server;
+
+/**
+ * Created by klaus on 5/30/15.
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
